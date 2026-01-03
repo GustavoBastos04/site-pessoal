33d7f676c9df1c0f3355db09707187dcfbf54220
GustavoBastos04 <gubasouza2004@gmail.com>
2026-01-03 20:57:16 -0300

    Add Media Query Hook

---
+++ site-pessoal/src/components/Header/UseMediaQueryHook/UseMediaQueryHook.tsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react';
+
+function UseMediaQueryHook(query: string){
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+        const media = window.matchMedia(query);
+    
+        setMatches(media.matches)
+
+        const listener = () => setMatches(media.matches);
+
+        media.addEventListener("change", listener);
+
+        return () => media.removeEventListener("change", listener)
+    }, [query]);
+
+    return matches;
+    
+}
+
+export default UseMediaQueryHook;
